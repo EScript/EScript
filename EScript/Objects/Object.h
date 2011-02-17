2135aaf8f8938be0ca87f5524b93eeb0c018fd59
Claudius Jähn <claudiusj@live.de>
2011-02-17 18:35:48 +0000

    EScript/Objects/Object.h added as an alias (for 0.5.3 compatibility).
    git-svn-id: https://svn.berlios.de/svnroot/repos/escript/branches/0.5.2.Stoepsel@11 5693b1fc-3b75-4070-9b6b-3ce3692a40d5

---
+++ EScript/Objects/Object.h
@@ -0,0 +1,8 @@
+// Object.h
+// This file is part of the EScript programming language.
+// See copyright notice in EScript.h
+// ------------------------------------------------------
+
+/* 	This file "EScript/Objects/Object.h" is just an alias for "EScript/Object.h".
+	This should improve compatibility with version 0.5.3 */
+#include "../Object.h"
