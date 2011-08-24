4da681bc3c5547b0c2bdf9f7e7a0b8a0bc2e8cec
Benjamin Eikel <benjamin@eikel.org>
2011-08-24 13:29:35 +0000

    Move declaration and runtime helper functions from EScript.(h|cpp) to Helper.(h|cpp).
    git-svn-id: https://svn.berlios.de/svnroot/repos/escript/branches/0.5.6.stable@98 5693b1fc-3b75-4070-9b6b-3ce3692a40d5

--- EScript/EScript.cpp
+++ EScript/EScript.cpp
@@ -3,7 +3,6 @@
 // See copyright notice in EScript.h
 // ------------------------------------------------------
 #include "EScript.h"
-#include "Objects/Internals/Block.h"
 #include "Objects/Identifier.h"
 #include "Objects/YieldIterator.h"
 #include "Parser/Parser.h"
@@ -15,13 +14,10 @@
 #include "../E_Libs/IOLib.h"
 #include "../E_Libs/MathLib.h"
 
-
-#include <sstream>
-
-using namespace EScript;
+namespace EScript {
 
 //! (static)
-void EScript::init() {
+void init() {
 	Namespace * SGLOBALS=getSGlobals();
 
 	Object::init(*SGLOBALS);
@@ -60,181 +56,14 @@ void EScript::init() {
 }
 
 //! (static,internal)
- Namespace * EScript::getSGlobals() {
+ Namespace * getSGlobals() {
 	static ERef<Namespace> sglobals(new Namespace);
 	return sglobals.get();
 }
 
-
 //! (static)
-void EScript::initLibrary(libInitFunction * initFunction) {
+void initLibrary(libInitFunction * initFunction) {
 	(*initFunction)(getSGlobals());
 }
 
-//! (static,internal)
-void EScript::assertParamCount_2(Runtime & runtime,const ParameterValues & params,int min,int max) {
-	if (min>=0 && static_cast<int>(params.count())<min) {
-		std::ostringstream sprinter;
-		sprinter<<"Too few parameters: Expected " <<min<<", got "<<params.count()<<".";
-		Object * c=runtime.getCallingObject();
-		if(c) sprinter << c->toString();
-		runtime.error(sprinter.str());
-	} else  if (max>=0 &&static_cast<int>(params.count())>max) {
-		std::ostringstream sprinter;
-		sprinter<<"Too many parameters: Expected " <<max<<", got "<<params.count()<<".";
-		Object * c=runtime.getCallingObject();
-		if(c) sprinter << c->toString();
-		runtime.warn(sprinter.str());
-	}
 }
-
-//! (static,internal) Non-inline part of assertType(...)
-void EScript::assertType_throwError(Runtime & runtime,const ObjPtr & obj){
-	runtime.error("Wrong Object Type.",obj.get());
-}
-
-//! (static)
-Object * EScript::callMemberFunction(Runtime & rt,ObjPtr obj,identifierId fnNameId,const ParameterValues & params) {
-	if (obj.isNull()) return NULL;
-	return rt.executeFunction(obj->getAttribute(fnNameId),obj.get(),params);
-}
-
-//! (static)
-Object * EScript::callMemberFunction(Runtime & rt,ObjPtr obj,const std::string & fnName,const ParameterValues & params) {
-	return callMemberFunction(rt,obj,stringToIdentifierId(fnName),params);
-}
-
-//! (static)
-Object * EScript::callFunction(Runtime & rt,Object * function,const ParameterValues & params) {
-	if (!function) return NULL;
-	return rt.executeFunction(function,NULL,params);;
-}
-
-//! (static)
-void EScript::declareFunction( Type * t,identifierId nameId, Function::functionPtr fn){
-	t->setTypeAttribute(nameId,new Function(nameId,0,-1,fn));
-}
-
-//! (static)
-void EScript::declareFunction( Type * t,const char *name, Function::functionPtr fn ){
-	declareFunction(t,stringToIdentifierId(name),fn);
-}
-
-//! (static)
-void EScript::declareFunction( Type * t,identifierId nameId, int minParamCount,int maxParamCount,Function::functionPtr fn){
-	t->setTypeAttribute(nameId,new Function(nameId,minParamCount,maxParamCount,fn));
-}
-
-//! (static)
-void EScript::declareFunction( Type * t,const char *name,int minParamCount,int maxParamCount, Function::functionPtr fn ){
-	declareFunction(t,stringToIdentifierId(name),minParamCount,maxParamCount,fn);
-}
-
-//! (static)
-void EScript::declareConstant( Type * type,const char *name, Object * value){
-	declareConstant(type,stringToIdentifierId(name),value);
-}
-
-//! (static)
-void EScript::declareConstant( Type * type,identifierId nameId, Object * value){
-	type->setTypeAttribute(nameId,value);
-}
-
-//! (static)
-void EScript::declareFunction( Namespace * t,identifierId nameId, Function::functionPtr fn){
-	t->setObjAttribute(nameId,new Function(nameId,0,-1,fn));
-}
-
-//! (static)
-void EScript::declareFunction( Namespace * t,const char *name, Function::functionPtr fn ){
-	declareFunction(t,stringToIdentifierId(name),fn);
-}
-
-//! (static)
-void EScript::declareFunction( Namespace * t,identifierId nameId, int minParamCount,int maxParamCount, Function::functionPtr fn){
-	t->setObjAttribute(nameId,new Function(nameId,minParamCount,maxParamCount,fn));
-}
-
-//! (static)
-void EScript::declareFunction( Namespace * t,const char *name, int minParamCount,int maxParamCount, Function::functionPtr fn ){
-	declareFunction(t,stringToIdentifierId(name),minParamCount,maxParamCount,fn);
-}
-
-//! (static)
-void EScript::declareConstant( Namespace * type,const char *name, Object * value){
-	declareConstant(type,stringToIdentifierId(name),value);
-}
-
-//! (static)
-void EScript::declareConstant( Namespace * type,identifierId nameId, Object * value){
-	type->setObjAttribute(nameId,value);
-}
-
-//! (static)
-void EScript::out(Object * obj) {
-	if (!obj)
-		std::cout << "NULL";
-	else
-		std::cout << obj->toString();
-}
-
-//! (static)
-Block * EScript::loadScriptFile(const std::string & filename,ERef<Block> block)throw(Exception *) {
-	if(block.isNull())
-		block=new Block;
-	ERef<Parser> pRef=new Parser;
-	try{
-		pRef->parseFile(block.get(),filename);
-	}catch(Exception * e){
-	block=NULL;
-	pRef=NULL;
-		throw (e);
-	}
-	return block.detachAndDecrease();
-}
-
-//! (static)
-std::pair<bool,ObjRef> EScript::loadAndExecute(Runtime & runtime,const std::string & filename){
-	ObjRef script;
-	try {
-		script = EScript::loadScriptFile(filename);
-	} catch (Exception * error) {
-		std::cerr << "\nError occured while loading file "<<filename<<" : \n" << error->toString() << std::endl;
-		return std::make_pair(false,error);
-	}
-	bool success=true;
-	ObjRef result;
-	try {
-		runtime.executeObj(script.get());
-		result=runtime.getResult();
-		if(runtime.getState()==Runtime::STATE_EXCEPTION){
-			std::cout <<"\nException caught (1): \n" << result.toString() <<std::endl;;
-			success=false;
-		}
-
-	}catch (Object * o) {
-		result=o;
-		std::cout <<"\nException caught (2): \n" << result.toString() <<std::endl;
-		success=false;
-	}catch (...) {
-		std::cout << "\nCaught unknown C++ expception. "<<std::endl;
-		success=false;
-	}
-	return std::make_pair(success,result);
-}
-
-////! (static)
-//Map * EScript::getAttributesAsMap(Object * obj){
-//    if(!obj)
-//		return NULL;
-//
-//	std::map<identifierId,Object *> attr;
-//	obj->getAttributes(attr);
-//
-//    Map *m=new Map();
-//
-//    for(std::map<identifierId,Object *>::iterator it=attr.begin();it!=attr.end();++it){
-//        m->setValue(String::create( identifierIdToString(it->first) ), it->second->getRefOrCopy());
-//    }
-//    return m;
-//}
---
+++ EScript/Helper.cpp
@@ -0,0 +1,178 @@
+// Helper.cpp
+// This file is part of the EScript programming language.
+// See copyright notice in EScript.h
+// ------------------------------------------------------
+#include "Helper.h"
+#include "Objects/Exception.h"
+#include "Objects/Namespace.h"
+#include "Objects/Object.h"
+#include "Objects/Type.h"
+#include "Parser/Parser.h"
+#include "Runtime/Runtime.h"
+#include "Utils/Hashing.h"
+#include <sstream>
+
+namespace EScript {
+
+//! (static)
+void declareFunction(Type * type, identifierId nameId, Function::functionPtr fn) {
+	type->setTypeAttribute(nameId, new Function(nameId, 0, -1, fn));
+}
+
+//! (static)
+void declareFunction(Type * type, const char * name, Function::functionPtr fn) {
+	declareFunction(type, stringToIdentifierId(name), fn);
+}
+
+//! (static)
+void declareFunction(Type * type, identifierId nameId, int minParamCount, int maxParamCount, Function::functionPtr fn) {
+	type->setTypeAttribute(nameId, new Function(nameId, minParamCount, maxParamCount, fn));
+}
+
+//! (static)
+void declareFunction(Type * type, const char * name, int minParamCount, int maxParamCount, Function::functionPtr fn) {
+	declareFunction(type, stringToIdentifierId(name), minParamCount, maxParamCount, fn);
+}
+
+//! (static)
+void declareConstant(Type * type, const char * name, Object * value) {
+	declareConstant(type, stringToIdentifierId(name), value);
+}
+
+//! (static)
+void declareConstant(Type * type, identifierId nameId, Object * value) {
+	type->setTypeAttribute(nameId, value);
+}
+
+//! (static)
+void declareFunction(Namespace * nameSpace, identifierId nameId, Function::functionPtr fn) {
+	nameSpace->setObjAttribute(nameId, new Function(nameId, 0, -1, fn));
+}
+
+//! (static)
+void declareFunction(Namespace * nameSpace, const char * name, Function::functionPtr fn) {
+	declareFunction(nameSpace, stringToIdentifierId(name), fn);
+}
+
+//! (static)
+void declareFunction(Namespace * nameSpace, identifierId nameId, int minParamCount, int maxParamCount, Function::functionPtr fn) {
+	nameSpace->setObjAttribute(nameId, new Function(nameId, minParamCount, maxParamCount, fn));
+}
+
+//! (static)
+void declareFunction(Namespace * nameSpace, const char * name, int minParamCount, int maxParamCount, Function::functionPtr fn) {
+	declareFunction(nameSpace, stringToIdentifierId(name), minParamCount, maxParamCount, fn);
+}
+
+//! (static)
+void declareConstant(Namespace * nameSpace, const char * name, Object * value) {
+	declareConstant(nameSpace, stringToIdentifierId(name), value);
+}
+
+//! (static)
+void declareConstant(Namespace * nameSpace, identifierId nameId, Object * value) {
+	nameSpace->setObjAttribute(nameId, value);
+}
+
+//! (static, internal)
+void assertParamCount_2(Runtime & runtime, int paramCount, int min, int max) {
+	if (min >= 0 && paramCount < min) {
+		std::ostringstream sprinter;
+		sprinter << "Too few parameters: Expected " << min << ", got " << paramCount << ".";
+		Object * object = runtime.getCallingObject();
+		if(object != NULL) {
+			sprinter << object->toString();
+		}
+		runtime.error(sprinter.str());
+	} else  if (max >= 0 && paramCount > max) {
+		std::ostringstream sprinter;
+		sprinter << "Too many parameters: Expected " << max << ", got " << paramCount << ".";
+		Object * object = runtime.getCallingObject();
+		if(object != NULL) {
+			sprinter << object->toString();
+		}
+		runtime.warn(sprinter.str());
+	}
+}
+
+//! (static, internal) Non-inline part of assertType(...)
+void assertType_throwError(Runtime & runtime, const ObjPtr & obj) {
+	runtime.error("Wrong Object Type.", obj.get());
+}
+
+//! (static)
+Object * callMemberFunction(Runtime & rt, ObjPtr obj, identifierId fnNameId, const ParameterValues & params) {
+	if (obj.isNull()) {
+		return NULL;
+	}
+	return rt.executeFunction(obj->getAttribute(fnNameId), obj.get(), params);
+}
+
+//! (static)
+Object * callMemberFunction(Runtime & rt, ObjPtr obj, const std::string & fnName, const ParameterValues & params) {
+	return callMemberFunction(rt, obj, stringToIdentifierId(fnName), params);
+}
+
+//! (static)
+Object * callFunction(Runtime & rt, Object * function, const ParameterValues & params) {
+	if (function == NULL) {
+		return NULL;
+	}
+	return rt.executeFunction(function, NULL, params);;
+}
+
+
+//! (static)
+void out(Object * obj) {
+	if (obj == NULL) {
+		std::cout << "NULL";
+	} else {
+		std::cout << obj->toString();
+	}
+}
+
+//! (static)
+Block * loadScriptFile(const std::string & filename, ERef<Block> block) throw (Exception *) {
+	if(block.isNull()) {
+		block = new Block;
+	}
+	Parser parser;
+	try {
+		parser.parseFile(block.get(), filename);
+	} catch(Exception * e) {
+		block = NULL;
+		throw (e);
+	}
+	return block.detachAndDecrease();
+}
+
+//! (static)
+std::pair<bool, ObjRef> loadAndExecute(Runtime & runtime, const std::string & filename) {
+	ObjRef script;
+	try {
+		script = loadScriptFile(filename);
+	} catch (Exception * error) {
+		std::cerr << "\nError occured while loading file \"" << filename << "\":\n" << error->toString() << std::endl;
+		return std::make_pair(false, error);
+	}
+	bool success = true;
+	ObjRef result;
+	try {
+		runtime.executeObj(script.get());
+		result = runtime.getResult();
+		if(runtime.getState() == Runtime::STATE_EXCEPTION) {
+			std::cout << "\nException caught (1):\n" << result.toString() << std::endl;
+			success = false;
+		}
+	} catch (Object * o) {
+		result = o;
+		std::cout << "\nException caught (2):\n" << result.toString() << std::endl;
+		success = false;
+	} catch (...) {
+		std::cout << "\nCaught unknown C++ exception." << std::endl;
+		success = false;
+	}
+	return std::make_pair(success, result);
+}
+
+}
