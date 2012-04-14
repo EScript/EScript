284134f6ec1c187ccae11a0e96d088b534b135ea
Claudius Jähn <claudiusj@live.de>
2012-04-14 13:37:43 +0000

    Version info updated.
    git-svn-id: https://svn.berlios.de/svnroot/repos/escript/branches/0.6.2.stable@325 5693b1fc-3b75-4070-9b6b-3ce3692a40d5

--- EScript/Version.h
+++ EScript/Version.h
@@ -5,12 +5,12 @@
 #ifndef ES_VERSION_H
 
 // huge(always 0) * 10000 + major * 100 + minor
-#define ES_VERSION 601 
-#define ES_VERSION_STRING "EScript 0.6.1 GubbelGubbel - exp (http://escript.berlios.de/)\0"
+#define ES_VERSION 602 
+#define ES_VERSION_STRING "EScript 0.6.2 GubbelGubbel - stable (http://escript.berlios.de/)\0"
 #define ES_VERSION_NAME "GubbelGubbel\0"
 
 // used for the windows resources file:
-#define ES_VERSION_RES 0,6,1,0
-#define ES_VERSION_RES_STRING "0,6,1,0\0"
+#define ES_VERSION_RES 0,6,2,0
+#define ES_VERSION_RES_STRING "0,6,2,0\0"
 
 #endif // ES_VERSION_H
