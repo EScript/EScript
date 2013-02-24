a481fa7c61e808f588aa9f1264d0117599266f40
Claudius Jähn <claudiusj@live.de>
2013-02-24 18:20:23 +0000

    StdFactories: Fixed issue where a EScript::create(SomeClass*) could result in creating an object of type Bool instead of E_SomeClass.
    git-svn-id: https://svn.berlios.de/svnroot/repos/escript/branches/0.6.6.stable@466 5693b1fc-3b75-4070-9b6b-3ce3692a40d5

--- EScript/Utils/StdFactories.h
+++ EScript/Utils/StdFactories.h
@@ -9,6 +9,7 @@
 #include <cstdint>
 #include <vector>
 #include <string>
+#include <type_traits>
 
 namespace EScript {
 
@@ -20,17 +21,33 @@ class StringData;
 class Bool;
 class Void;
 
+/*! Factories for types that are not results of unwanted implicit type conversions. */
 Array* create(const std::vector<Object*>&);
-Number* create(const double&);
-Number* create(const float&);
-Number* create(const int&);
-Number* create(const uint32_t&);
 String* create(const StringData &);
 String* create(const std::string &);
 String* create(const char*);
-Bool* create(const bool&);
 Void* create(std::nullptr_t);
 
+/*!	For primitive types that can be the result of an implicit type conversion, 
+	a template function is used that requires the exact type.
+	This allows detecting errors if EScript::create(SomeType*) is
+	not specified for "SomeType*" (or the corresponding include is missing) and
+	the implicit cast would then result in creating an object of type Bool.	*/
+namespace _Internals{
+Number * createNumber(double v);
+Number * createNumber(float v);
+Number * createNumber(int v);
+Number * createNumber(uint32_t v);
+Bool * createBool(bool v);
+}
+
+template<class T ,class = typename std::enable_if<std::is_same<T,bool>::value>::type>
+Bool * create(T value)			{	return _Internals::createBool(value);	}
+
+template<class T ,class = typename std::enable_if<std::is_same<T,double>::value || 
+		std::is_same<T,float>::value || std::is_same<T,int>::value || 
+		std::is_same<T,uint32_t>::value>::type>
+Number * create(T value)		{	return _Internals::createNumber(value);	}
 }
 
 #endif // ES_STD_FACTORIES_H_INCLUDED
