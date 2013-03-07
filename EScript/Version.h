bf089ca7b0a43c7c3bf2338f31de17a01cb5aaf7
Claudius Jähn <claudiusj@live.de>
2013-03-07 17:49:16 +0000

    Version info updated.
    git-svn-id: https://svn.berlios.de/svnroot/repos/escript/branches/0.6.7.experimental@473 5693b1fc-3b75-4070-9b6b-3ce3692a40d5

--- EScript/Version.h
+++ EScript/Version.h
@@ -5,12 +5,12 @@
 #ifndef ES_VERSION_H
 
 // huge(always 0) * 10000 + major * 100 + minor
-#define ES_VERSION 606
-#define ES_VERSION_STRING "EScript 0.6.6 Eduard (http://escript.berlios.de/)\0"
-#define ES_VERSION_NAME "Eduard\0"
+#define ES_VERSION 607
+#define ES_VERSION_STRING "EScript 0.6.7 Elise (http://escript.berlios.de/)\0"
+#define ES_VERSION_NAME "Elise\0"
 
 // used for the windows resources file:
-#define ES_VERSION_RES 0,6,6,0
-#define ES_VERSION_RES_STRING "0,6,6,0\0"
+#define ES_VERSION_RES 0,6,7,0
+#define ES_VERSION_RES_STRING "0,6,7,0\0"
 
 #endif // ES_VERSION_H
