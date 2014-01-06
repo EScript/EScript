5a01f78a6073b1cee01a676218db69418925e0bb
Claudius Jähn <claudiusj@live.de>
2014-01-06 20:08:03 +0000

    0.7.0 version info updated.
    git-svn-id: https://svn.berlios.de/svnroot/repos/escript/branches/0.7.0.Eli@536 5693b1fc-3b75-4070-9b6b-3ce3692a40d5

--- EScript/Version.h
+++ EScript/Version.h
@@ -1,7 +1,7 @@
 // Version.h
 // This file is part of the EScript programming language (http://escript.berlios.de)
 //
-// Copyright (C) 2012-2013 Claudius Jähn <claudius@uni-paderborn.de>
+// Copyright (C) 2012-2014 Claudius Jähn <claudius@uni-paderborn.de>
 // Copyright (C) 2012 Benjamin Eikel <benjamin@eikel.org>
 //
 // Licensed under the MIT License. See LICENSE file for details.
@@ -9,12 +9,12 @@
 #ifndef ES_VERSION_H
 
 // huge(always 0) * 10000 + major * 100 + minor
-#define ES_VERSION 607
-#define ES_VERSION_STRING "EScript 0.6.7 Elise (http://escript.berlios.de/)\0"
-#define ES_VERSION_NAME "Elise\0"
+#define ES_VERSION 700
+#define ES_VERSION_STRING "EScript 0.7.0 Eli (http://escript.berlios.de/)\0"
+#define ES_VERSION_NAME "Eli\0"
 
 // used for the windows resources file:
-#define ES_VERSION_RES 0,6,7,0
-#define ES_VERSION_RES_STRING "0,6,7,0\0"
+#define ES_VERSION_RES 0,7,0,0
+#define ES_VERSION_RES_STRING "0,7,0,0\0"
 
 #endif // ES_VERSION_H
