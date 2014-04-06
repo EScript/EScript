371c1c4bc349fdc9f82ec456f34892dbc58ab8d1
Claudius <>
2014-04-06 12:49:54 +0200

    Update version information.

--- EScript/Version.h
+++ EScript/Version.h
@@ -1,7 +1,7 @@
 // Version.h
-// This file is part of the EScript programming language (http://escript.berlios.de)
+// This file is part of the EScript programming language (https://github.com/EScript)
 //
-// Copyright (C) 2012-2014 Claudius Jähn <claudius@uni-paderborn.de>
+// Copyright (C) 2012-2014 Claudius Jähn <ClaudiusJ@live.de>
 // Copyright (C) 2012 Benjamin Eikel <benjamin@eikel.org>
 //
 // Licensed under the MIT License. See LICENSE file for details.
@@ -9,12 +9,12 @@
 #ifndef ES_VERSION_H
 
 // huge(always 0) * 10000 + major * 100 + minor
-#define ES_VERSION 700
-#define ES_VERSION_STRING "EScript 0.7.0 Eli (http://escript.berlios.de/)\0"
-#define ES_VERSION_NAME "Eli\0"
+#define ES_VERSION 701
+#define ES_VERSION_STRING "EScript 0.7.1 Elfriede (https://github.com/EScript)\0"
+#define ES_VERSION_NAME "Elfriede\0"
 
 // used for the windows resources file:
-#define ES_VERSION_RES 0,7,0,0
-#define ES_VERSION_RES_STRING "0,7,0,0\0"
+#define ES_VERSION_RES 0,7,1,0
+#define ES_VERSION_RES_STRING "0,7,1,0\0"
 
 #endif // ES_VERSION_H
