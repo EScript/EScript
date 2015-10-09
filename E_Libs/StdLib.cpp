3f85dd2bafafc2ee8cf59d8ae1dc518ad9064d50
Benjamin Eikel <benjamin@eikel.org>
2015-10-09 17:16:38 +0200

    VS fix: _execv does return intptr_t, no pointer

--- E_Libs/StdLib.cpp
+++ E_Libs/StdLib.cpp
@@ -344,7 +344,7 @@ void StdLib::init(EScript::Namespace * globals) {
 		}
 		argv[argc] = nullptr;
 
-		Number * result = create(*_execv(parameter[0].toString().c_str(), argv));
+		Number * result = create(_execv(parameter[0].toString().c_str(), argv));
 
 		for(uint_fast32_t i = 0; i < argc; ++i) {
 			delete [] argv[i];
