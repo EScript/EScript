0f5f71eeb4086e9993981cd1cba2cae64f006f38
Benjamin Eikel <benjamin@eikel.org>
2015-10-09 17:26:35 +0200

    Define EXECV depending on function to use
    Leads to less duplicate code.

--- E_Libs/StdLib.cpp
+++ E_Libs/StdLib.cpp
@@ -24,8 +24,10 @@
 
 #if defined(_MSC_VER)
 #include <process.h>
+#define EXECV _execv
 #else
 #include <unistd.h>
+#define EXECV execv
 #endif
 
 #if defined(_WIN32)
@@ -329,7 +331,6 @@ void StdLib::init(EScript::Namespace * globals) {
 	//!	[ESF]  number system(command)
 	ES_FUN(globals,"system",1,1,system(parameter[0].toString().c_str()))
 
-#if defined(_MSC_VER)
 	//!	[ESF] Number exec(String path, Array argv)
 	ES_FUNCTION(globals, "exec", 2, 2, {
 		Array * array = assertType<Array>(rt, parameter[1]);
@@ -344,7 +345,7 @@ void StdLib::init(EScript::Namespace * globals) {
 		}
 		argv[argc] = nullptr;
 
-		Number * result = create(_execv(parameter[0].toString().c_str(), argv));
+		Number * result = create(EXECV(parameter[0].toString().c_str(), argv));
 
 		for(uint_fast32_t i = 0; i < argc; ++i) {
 			delete [] argv[i];
@@ -353,31 +354,6 @@ void StdLib::init(EScript::Namespace * globals) {
 
 		return result;
 	})
-#else
-	//!	[ESF] Number exec(String path, Array argv)
-	ES_FUNCTION(globals, "exec", 2, 2, {
-		Array * array = assertType<Array>(rt, parameter[1]);
-		uint32_t argc = array->size();
-
-		char ** argv  = new char *[argc + 1];
-		for(uint_fast32_t i = 0; i < argc; ++i) {
-			 std::string arg = array->get(i)->toString();
-			 argv[i] = new char[arg.length() + 1];
-			 std::copy(arg.begin(), arg.end(), argv[i]);
-			 argv[i][arg.length()] = '\0';
-		}
-		argv[argc] = nullptr;
-
-		Number * result = create(execv(parameter[0].toString().c_str(), argv));
-
-		for(uint_fast32_t i = 0; i < argc; ++i) {
-			delete [] argv[i];
-		}
-		delete [] argv;
-
-		return result;
-	})
-#endif
 
 	//! [ESF]  number time()
 	ES_FUN(globals,"time",0,0,static_cast<double>(time(nullptr)))
